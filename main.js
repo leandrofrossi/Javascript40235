7fcf095fb5d92f5140a7fc3a3baa5f3e66f41281
Leandro Rossi <leanmatador@gmail.com>
2022-09-28 18:02:58 -0300

    agregando sweetalerts

--- main.js
+++ main.js
@@ -32,17 +32,35 @@ function menorEdad(cliente){
     let formulario = document.querySelector("#inicio");
     formulario.innerHTML = "";
     let nuevoContenido = document.createElement("div");
-    nuevoContenido.innerHTML = `<p>Lo sentimos ${cliente.nombre}, pero tu edad no alcanza para ingresar a nuestra tienda. Volvé cuando seas mayor de 18 años.</p>`;
+    nuevoContenido.innerHTML = `<p>Lo sentimos <span>${cliente.nombre}</span>, pero tu edad no alcanza para ingresar a nuestra tienda.</p>
+    <p>Volvé cuando seas mayor de 18 años.</p>`;
 
     nuevoContenido.className = "ingreso";
     document.body.appendChild(nuevoContenido);
+    Swal.fire({
+        icon: 'error',
+        title: 'Ingreso Prohibido',
+        text: 'El ingreso solo está permitido para mayores de 18 años',        
+      })
 }
 function saludo(cliente) {
     let form = document.querySelector("#inicio");
     form.innerHTML = "";
     let nuevoContenido = document.createElement("div");
-    nuevoContenido.innerHTML = `<p>Hola ${cliente.nombre}. Bienvenido a nuestra tienda de vinos.</p>
+    nuevoContenido.innerHTML = `<p>Hola <span>${cliente.nombre}</span>.</p>
     <button class="boton" onClick="ingresoTienda()">Continuar</button>`;
+    Swal.fire({
+        title: '¡Hola!',        
+        text: "Bienvenido a nuestra tienda de vinos.",
+        imageUrl: '/imagenes/imagen-brindis.jpg',
+        imageWidth: 450,
+        imageHeight: 300,
+        imageAlt: 'Custom image',        
+        confirmButtonColor: '#3085d6',        
+        confirmButtonText: 'Ingresar'
+    }).then((result) => {
+        result.isConfirmed && ingresoTienda()          
+    });
 
     nuevoContenido.className = "ingreso";
     document.body.appendChild(nuevoContenido);
@@ -77,23 +95,41 @@ function agregarCarrito(id){
         prodAgregar.cantidad=1;
         carrito.push(prodAgregar);
         storage(carrito);
-        Swal.fire({
+        const Toast = Swal.mixin({
+            toast: true,
             position: 'top-end',
-            icon: 'success',
-            title: 'Producto agregado al carrito',
             showConfirmButton: false,
-            timer: 1000
-          });        
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'success',
+            title: 'Producto agregado al carrito'
+          })        
     } else {
         carrito[carritoId].cantidad +=1; 
         storage(carrito); 
-        Swal.fire({
+        const Toast = Swal.mixin({
+            toast: true,
             position: 'top-end',
-            icon: 'success',
-            title: 'Producto agregado al carrito',
             showConfirmButton: false,
-            timer: 1000
-          });        
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'success',
+            title: 'Producto agregado al carrito'
+          })         
     }
 }
 function storage(carrito){
@@ -107,15 +143,14 @@ function mostrarCarrito(){
     main.appendChild(divCarrito);
     if(carrito.length>0){
         carrito.forEach((producto,id)=>{            
-            //total = total + producto.precio*producto.cantidad;
             const carritoCard = document.createElement("div");
             carritoCard.className = "carritoCard";
             carritoCard.innerHTML = `
             <img src="${producto.imagen}" class="cart-img">
-            <div class="card-body">Producto: ${producto.variedad}</div>
-            <div class="card-body">Cantidad: ${producto.cantidad}</div>
-            <div class="card-body">$ ${producto.precio}</div>
-            <div class="card-body">Subtotal: ${producto.precio*producto.cantidad}</div>
+            <div class="card-body">Producto: <span>${producto.variedad}</span></div>
+            <div class="card-body">Cantidad: <span>${producto.cantidad}</span></div>
+            <div class="card-body">$ <span>${producto.precio}</span></div>
+            <div class="card-body">Subtotal: <span>${producto.precio*producto.cantidad}</span></div>
             <button class="boton" onClick="productDelete(${id})">Borrar Producto</button>
             `
             divCarrito.appendChild(carritoCard)
@@ -143,17 +178,51 @@ function mostrarCarrito(){
     }
 
 }
-function productDelete(id){         
-    carrito.splice(id,1);
-    total = 0;
-    storage(carrito);
-    mostrarCarrito();  
+function productDelete(id){  
+    Swal.fire({
+        title: '¿Eliminar producto?',
+        text: "Se eliminará el producto del carrito definitivamente",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar.'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                '¡Eliminado!',
+                'El producto fue eliminado del carrito',
+                'success'
+            );
+            carrito.splice(id,1);
+            total = 0;
+            storage(carrito);
+            mostrarCarrito();        
+        }
+    }) 
 }
 function borrarCarrito(){
-    carrito=[];
-    total = 0;
-    storage(carrito);
-    mostrarCarrito();
+    Swal.fire({
+        title: '¿Estas seguro/a?',
+        text: "Se eliminarán todos los productos del carrito.",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, vaciar.'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                '¡Vaciado!',
+                'Productos eliminados, el carrito ahora está vacío.',
+                'success'
+            );
+            carrito=[];
+            total = 0;
+            storage(carrito);
+            mostrarCarrito();
+        }
+    })    
 }
 function seguirComprando(){
     main.innerHTML = "";
@@ -174,33 +243,72 @@ function seguirComprando(){
     });
 }
 function finalizarCompra(){
-    main.innerHTML = "";
-    const finCompra = document.createElement("section")
-    finCompra.className = "finCompra";
-    finCompra.innerHTML = `<p>Completar formulario para finalizar la compra.</p>
-        <div class="form">                
-            <div class="column">            
-                <input type="text" class="form-control" placeholder="juan.perez" aria-label="Username">
-                <span class="input-group-text">@</span>
-                <input type="text" class="form-control" placeholder="gmail.com" aria-label="Server"> 
-                <label for="inputAddress" class="form-label">DIRECCION</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="Calle Falsa 123">                
-                <label for="tel">TELEFONO</label>
-                <input class="field" type="text" name="tel" id="tel" autocomplete="off"placeholder="1123456789">                    
-                <button class="boton" onClick="saludoFinal()" id="send">Enviar</button>                
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: '¿Desea finalizar la compra?',        
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, finalizar.',
+        cancelButtonText: 'No, seguir comprando.',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            '¡Compra confirmada!',
+            'Formulario final a continuación.',
+            'success')
+            main.innerHTML = "";
+            const finCompra = document.createElement("section")
+            finCompra.className = "finCompra";
+            finCompra.innerHTML = `<h3>Completar formulario para finalizar la compra.</h3>
+            <p>El total de la compra es de $${total}.</p>
+            <div class="form">                
+                <div class="column">  
+                    <label for="mail">EMAIL</label>          
+                    <input type="text" class="form-control" placeholder="juan.perez@gmail.com" aria-label="mail">                
+                    <label for="inputAddress" class="form-label">DIRECCION</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder="Calle Falsa 123">                
+                    <label for="tel">TELEFONO</label>
+                    <input class="field" type="text" name="tel" id="tel" autocomplete="off"placeholder="1123456789">                    
+                    <button class="boton" onClick="saludoFinal()" id="send">Enviar</button>                
+                </div> 
             </div> 
-        </div> `
-    main.appendChild(finCompra);
-    carrito=[];
-    total = 0;     
+            <p>Por favor chequear que los datos sean correctos. Enviaremos un email con el resumen de la compra en el cual se especificará el día y horario de entrega en el domicilio proporcionado.</p>
+            `
+        main.appendChild(finCompra);    
+          
+        } else if (          
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Continuando compra',
+            'Puede seguir eligiendo productos.',
+            'info'
+          )
+          seguirComprando()
+        }
+      })          
 }
 function saludoFinal(){
+    let mail = document.querySelector(".form-control").value;
     let usuario = JSON.parse(sessionStorage.getItem("cliente"));
     main.innerHTML = "";
     const saludoFinal = document.createElement("div")
     saludoFinal.className = "finCompra";
-    saludoFinal.innerHTML = `<h3>¡¡Muchas gracias ${usuario.nombre}!!</h3> 
-    <p>Recibirás el resumen de la compra al mail proporcionado.</p>
+    saludoFinal.innerHTML = `<h3>¡¡Muchas gracias <span>${usuario.nombre}</span>!!</h3> 
+    <p>Recibirás el resumen de la compra al email <span>${mail}</span>.</p>
     <p>¡Hasta la próxima!</p>`
     main.appendChild(saludoFinal)
+    carrito=[];
+    total = 0;
+    storage(carrito); 
+    cartBoton = document.querySelector("#btnCart");
+    cartBoton.remove();
 }
